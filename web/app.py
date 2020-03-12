dc2fed76bd8177bcbf18a1f1e4570ff8e58efd77
Shiao-li Green <shiao-li@minerva.kgi.edu>
2020-03-12 12:40:43 +0800

    fixed db, css linking, [unresolved] login to index route
    Extra '/' in path, change in folder structure for css styling, linking between login and sign up page works, [unresolved] successful submission of login to index page.

--- web/app.py
+++ web/app.py
@@ -9,46 +9,50 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-path = os.path.abspath(os.getcwd()) + '\\app.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + path
+path = os.path.abspath(os.getcwd() + '\\app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + path
 
 db = SQLAlchemy(app)
-db.create_all(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+login = LoginManager()
+login.init_app(app)
 
 class User(db.Model):
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(200), unique=True)
-    password = db.Column(db.String(200))
-    contact = db.Column(db.String(200))
+    firstName = db.Column(db.String(200))
+    lastName = db.Column(db.String(200))
+    password = db.Column(db.String(16))
+    # contact = db.Column(db.String(200))
+
+db.create_all()
+db.session.commit()
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 @app.route('/signup', methods=['GET','POST'])
-def sign_up():
+def signup():
     # when button to sign up on login page is clicked
     if request.method == 'GET':
-        return render_template('sign_up.html')
+        return render_template('signUp.html')
 
     elif request.method == 'POST':
         # return redirect(url_for('index'))
 
         # check that email is not already registered
-        user = db.session.query(User).filter(User.email==request.form['email']).first()
+        user = db.session.query(User).filter(User.email==request.form['Email']).first()
         if not user:
-            user = User(email=request.form['email'],password=request.form['password'],contact=request.form['contact'])
+            user = User(email=request.form['email'],password=request.form['pwd1'],firstName=request.form['firstName'], lastName=request.form['lastName'])
             db.session.add(user)
             db.session.commit()
             return redirect(url_for('index'))
         else:
             flash('This email already has an account.')
             # makes more sense to redirect a registered user to login page
-            return render_template('login.html')
+            return redirect(url_for('login'))
             # return render_template('sign_up.html')
 
 # route will depend on whether how we structure pages
@@ -59,7 +63,7 @@ def login():
         return render_template('login.html')
     elif request.method == 'POST':
         #return redirect(url_for('index'))
-        user = db.session.query(User).filter(User.email==request.form['email'], password=request.form['password']).first()
+        user = db.session.query(User).filter(User.email==request.form['email'], User.password=request.form['password']).first()
         # check that the user is in the database
         if not user:
             flash('This email does not have an account.')
@@ -71,14 +75,14 @@ def login():
 # @login_required
 def index():
     ## take login as the default index pages
-    return render_template('index.html')
+    return redirect(url_for('login'))
 
 @app.route('/logout', methods=['GET'])
 @login_required
 def log_out():
     logout_user()
-    return redirect(url_for(('login'))
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    sess.init_app(app)
+    # session.init_app(app)
     app.run(debug=True)
