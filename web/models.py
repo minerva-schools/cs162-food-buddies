990698282ced993760c70e8a353bc58aa5a0310b
Jingren Wang <jingren.wang@minerva.kgi.edu>
2020-04-14 19:52:12 +0800

    removed unique constraints on user name
    Removed unique constraints for the first_name & last_name columns of the User table

--- web/models.py
+++ web/models.py
@@ -12,12 +12,16 @@ class MinervaCities(enum.Enum):
     London = 'London'
     Taipei = 'Taipei'
 
+
 class User(db.Model,UserMixin):
+    '''The User table store user name, city of residence, and contact info'''
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100), unique=True)
-    last_name = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(100), unique=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True) # each Minervan has a unique gmail address
     password = db.Column(db.String(200))
     city = db.Column(db.Enum(MinervaCities))
-    #contact = db.Column(db.String(200))
+    # contact ?? can add facebook accounts
+
+
