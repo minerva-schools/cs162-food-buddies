5bce50617a83b46407f448824541bf5c722b0967
Shiao-li Green <shiao-li@minerva.kgi.edu>
2020-04-19 13:16:07 +0100

    Update 5b2f3bdef44e_.py

--- migrations/versions/5b2f3bdef44e_.py
+++ migrations/versions/5b2f3bdef44e_.py
@@ -24,7 +24,7 @@ def upgrade():
     sa.Column('last_name', sa.String(length=100), nullable=True),
     sa.Column('email', sa.String(length=100), nullable=True),
     sa.Column('password', sa.String(length=200), nullable=True),
-    sa.Column('city', sa.Enum('SanFrancisco', 'Seoul', 'Hyderabad', 'Berlin', 'BuenosAires', 'London', 'Taipei', name='minervacities'), nullable=True),
+    sa.Column('city', sa.Enum('SanFrancisco', 'Seoul', 'Hyderabad', 'Berlin', 'BuenosAires', 'London', 'Taipei', name='minervacities', create_type=False), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email')
     )
