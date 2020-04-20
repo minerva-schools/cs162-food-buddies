5c0d25d58f29d74a4a2c7bbd16222feb10630abe
Shiao-li Green <shiao-li@minerva.kgi.edu>
2020-04-20 22:28:16 +0100

    remove changes for heroku migration
    ensure no migration changes are transferred to dev

--- web/__init__.py
+++ web/__init__.py
@@ -3,11 +3,8 @@ from flask_login import LoginManager
 import os
 from .config import config_by_name
 from web.models import db
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
 login_manager = LoginManager()
-migrate = Migrate()
 
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -23,10 +20,9 @@ def create_app(config_name='dev'):
     app.config.from_object(config_by_name[config_name])
 
     db.init_app(app)
-    
+
 
     login_manager.init_app(app)
-    migrate.init_app(app,db)
 
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(401, custom_401)
@@ -39,5 +35,5 @@ def create_app(config_name='dev'):
 
         # Create DB Models
         db.create_all()
-        
+
     return app
