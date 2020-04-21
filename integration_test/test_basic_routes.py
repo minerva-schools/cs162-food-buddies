383426ededd210dbfe2fdb99cc61b19df69f2dc2
mudolucas <lucas.mudo@minerva.kgi.edu>
2020-04-20 23:58:08 -0400

    fixed typo

--- integration_test/test_basic_routes.py
+++ integration_test/test_basic_routes.py
@@ -11,7 +11,7 @@ def test_signup_request():
 def test_404_error_handler():
     r = requests.get('http://127.0.0.1:5000/this_page_is_fake')
     assert r.status_code == 404
-    assert "Etiher you requested invaild page or you don't have permission to access this page." in r.text
+    assert "Etiher you requested invalid page or you don't have permission to access this page." in r.text
 
 def test_redirect_preferences():
     r = requests.get('http://127.0.0.1:5000/preference')
