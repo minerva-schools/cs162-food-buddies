1aa8b50bfcafaa3d66b28e8a17265c1b8d38987e
Shiao-li Green <shiao-li@minerva.kgi.edu>
2020-04-22 16:52:23 +0100

    Update models.py

--- web/models.py
+++ web/models.py
@@ -5,12 +5,12 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 # create table schema
+
 class City(db.Model):
     '''Store current Minerva city names'''
     __tablename__ = 'City'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     city_name = db.Column(db.String(80))
-
 
 class User(db.Model, UserMixin):
     '''The User table store user name, city of residence, and contact info.
@@ -45,7 +45,8 @@ class Preference(db.Model):
     '''
     __tablename__ = 'Preference'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    date_time = db.Column(db.DateTime)
+    #date_time = db.Column(db.DateTime)
+    date_time=db.Column(db.String(100))
 
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
     city_id = db.Column(db.Integer, db.ForeignKey('City.id'))
@@ -59,4 +60,81 @@ class Preference(db.Model):
     require_gluten_free = db.Column(db.Boolean,default=False )
     require_dairy_free = db.Column(db.Boolean,default=False )
 
+    # time availability
+    # store start/end time of desired dining time as integer (0 - 24th hour of the day)
+    start_time = db.Column(db.String(100))
+    end_time = db.Column(db.String(100))
+
     matched = db.Column(db.Boolean, default=False) # True if a buddy is found
+
+
+
+# define event listeners
+# * each lister inserts default data to the specified table (only once) after db creation
+@db.event.listens_for(City.__table__, 'after_create', once=True)
+def insert_city_options(*args, **kwargs):
+    '''insert Minerva city options upon db creation'''
+    MinervaCities = ['San Francisco', 'Seoul', 'Hyderabad', 'Berlin', 'Buenos Aires', 'London', 'Taipei']
+    for city_name in MinervaCities:
+        db.session.add(City(city_name=city_name))
+    db.session.commit()
+
+@db.event.listens_for(Cuisine.__table__, 'after_create', once=True)
+def insert_cuisine_options(*args, **kwargs):
+    '''insert cuisine options upon db creation'''
+    CuisineTypes = ['American', 'Asian', 'European', 'Middle Eastern', 'Latin American', 'Subcontinental']
+    for cuisine_name in CuisineTypes:
+        db.session.add(Cuisine(cuisine_name=cuisine_name))
+    db.session.commit()
+
+@db.event.listens_for(DineTime.__table__, 'after_create', once=True)
+def insert_dinetime_options(*args, **kwargs):
+    '''insert dining time options upon db creation'''
+    DiningTimes = ['Lunch', 'Dinner', 'Breakfast']
+    for dinetime_name in DiningTimes:
+        db.session.add(DineTime(dinetime_name=dinetime_name))
+    db.session.commit()
+
+
+# dump in some dummy user data upon db creation (for demo purpose)
+@db.event.listens_for(User.__table__, 'after_create', once=True)
+def insert_dummy_users(*args, **kwargs):
+    '''insert dummy user data upon db creation'''
+    # hashed password strings
+    hashedPW = {   # keys are the actual passwords from dummy user input (please use them to login)
+        88888888: 'sha256$30buaXI6$085be32da5b90c6b3bb002e7ba4764c1f4f4fa070b7d263c870a621e55efe2ee',
+        77777777: 'sha256$ZLmXsoHR$cbd1ea13546619e8291686f46822c77a7e8099e5e7bc0095b1329a81f821fe8a',
+        66666666: 'sha256$7nZiviKc$21edefd8abe3f32baa7284a24a90dab793b5c94cf7a63cec5e1fb283d1160f3d',
+        55555555: 'sha256$V7vmFKKC$d2a91f308dc90375cf5b07021c5830cd593146ead60f757bc607c6cbfec6e4b2'
+        }
+
+    DummyUsers = [
+            [2,'Jingren', 'Wang', 'jingren.wang@minerva.kgi.edu',
+        hashedPW[88888888], 'Phone', '+82 105-557-7494'], # Seoul
+            [5,'Mary', 'Burns', 'mary.burns@minerva.kgi.edu',
+        hashedPW[77777777], 'Phone', '+54 155-5516-605'], # Buenos Aires
+            [1,'James', 'Gorden', 'james.gorden@minerva.kgi.edu',
+        hashedPW[66666666], 'Phone', '+1 729-591-1038'], # San Francisco
+            [1,'Kia', 'Louis', 'kia.louis@minerva.kgi.edu',
+        hashedPW[55555555], 'Phone', '+1 813-028-4012'] # San Francisco
+        ]
+
+    for user_info in DummyUsers:
+        db.session.add(User( city_id=user_info[0],
+            first_name=user_info[1], last_name=user_info[2],
+                email=user_info[3], password=user_info[4],
+                contact_method=user_info[5], contact_info=user_info[6]))
+    db.session.commit()
+
+
+# dump in some dummy preferences upon db creation
+@db.event.listens_for(Preference.__table__, 'after_create', once=True)
+def insert_dummy_preferences(*args, **kwargs):
+    '''insert one dummy user preference upon db creation'''
+    db.session.add(Preference(
+        date_time='2020-05-01 11:05:26',
+            user_id=2, city_id=5, cuisine_id=3, dinetime_id=1,
+            require_vegan=True, start_time='11:10', end_time='13:30'
+            ))
+            # *other 'require_' attributes default to Flase (0)
+    db.session.commit()
