9267cbeb81a17b90fd59c5727076e00e21950595
Jingren Wang <jingren.wang@minerva.kgi.edu>
2020-04-24 01:52:04 +0800

    backend: db updates upon user profile edits
    1. successful update on all user changes of personal info except user profile image. current feature supports one edit a time, followed by automatic reload of preference page ( with updated first name displayed, the rest data are updated in db, not shown)
    2. partial success on image upload (can see image changes on local file), however could not render updated file (same filename) on preference page.

--- web/app.py
+++ web/app.py
@@ -5,6 +5,9 @@ from flask import current_app as app
 from datetime import datetime
 from .models import db, User, City, Cuisine, Preference, DineTime
 
+# to save uploaded images to folder
+import os
+
 # create an app factory
 main_routes = Blueprint('main_route',__name__,template_folder='templates')
 
@@ -13,13 +16,58 @@ main_routes = Blueprint('main_route',__name__,template_folder='templates')
 def index():
     return render_template('login.html')
 
+
+def get_img_path():
+    '''helper function to get the path to user profile image'''
+    if current_user.profile_img_name: # if there is a user-uploaded image
+        image_name = current_user.profile_img_name # strings in db
+    else:
+        image_name = "profilePicPlaceholder.png"
+    # construct image path to user profile photo
+    image_path = os.path.join(app.root_path, 'static/userProfilePics',image_name)
+    return image_path
+
+
 @main_routes.route('/preference', methods=['GET','POST'])
 @login_required
 def preference():
-    if request.method == 'GET':
-        return render_template('preference.html', firstName=current_user.first_name.capitalize(),
-        lastName=current_user.last_name.capitalize(),email=current_user.email,contactInfo=current_user.contact_info)
-    elif request.method == 'POST':
+    if request.method == 'GET' :
+        print('request.method == GET')
+        print(f'image_path = {get_img_path()}')
+        return render_template('preference.html',
+            firstName=current_user.first_name.capitalize(),
+            lastName=current_user.last_name.capitalize(),
+            email=current_user.email,
+            contactInfo=current_user.contact_info,
+            imagePath=get_img_path()
+           )
+        
+    elif request.method == 'POST' :
+
+        # if a profile photo is uploaded through a POST method before
+        if request.files['new_img']:
+            file = request.files['new_img'] # the File object (from Flask request)
+            srcfile_name = file.filename.lower()
+            ext = os.path.splitext(srcfile_name)[-1].lower() # get the extension
+            filename_db = 'user_'+str(current_user.id)+ext # the filename stored in db
+            # save this new file/ replace the target file with the same name
+            file.save(os.path.join(app.root_path, 'static/userProfilePics',filename_db))
+            # update corresponding file_name in db upon first upload
+            if current_user.profile_img_name==None:
+                 db.session.query(User).filter(User.id==current_user.id).update({'profile_img_name': filename_db})
+                 db.session.commit() # commit any changes
+            
+            print('request.method == POST')
+            print(f'image_path = {get_img_path()}')
+            return render_template('preference.html',
+                        firstName=current_user.first_name.capitalize(),
+                        lastName=current_user.last_name.capitalize(),
+                        email=current_user.email,
+                        contactInfo=current_user.contact_info,
+                        imagePath=get_img_path())
+
+        
+        # else, the POST cnocerns preference updates
         # check whether user preferences are already set
         preference = db.session.query(Preference).filter(Preference.user_id == current_user.id).first()
         if preference: # update preferences
@@ -38,7 +86,7 @@ def preference():
             preference.start_time = request.form['ava_from']
             preference.end_time = request.form['ava_to']
             db.session.commit()
-
+            
         else: # set user preferences
             # determine linking to other tables
             cuisine = db.session.query(Cuisine).filter(Cuisine.cuisine_name==request.form.get('cuisine_selected')).first()
@@ -47,6 +95,7 @@ def preference():
             preference = Preference(date_time=datetime.utcnow(), user_id=current_user.id, cuisine_id=1, dinetime_id=dinetime.id, city_id=current_user.city_id, require_vegetarian=("vegetarian" in request.form),require_vegan=("vegan" in request.form),require_halal=("halal" in request.form),require_gluten_free=("glutenFree" in request.form),require_dairy_free=("dairyFree" in request.form),start_time=request.form['ava_from'],end_time=request.form['ava_to'])
             db.session.add(preference)
             db.session.commit()
+            
         # placeholder page to indicate form has been submitted.
         return render_template('404.html')
         # return redirect(url_for('main_route.results'))
@@ -56,8 +105,39 @@ def preference():
 @login_required
 def edit(update):
 
+    new_first_name = request.form.get('new_first_name')
+    if new_first_name:
+        db.session.query(User).filter(User.id==current_user.id).update({'first_name': new_first_name})
+        db.session.commit() # commit any changes
+        
+    new_last_name = request.form.get('new_last_name')
+    if new_last_name:
+        db.session.query(User).filter(User.id==current_user.id).update({'last_name': new_last_name})
+        db.session.commit() # commit any changes
+               
+    new_email =request.form.get('new_email')
+    if new_email:
+        db.session.query(User).filter(User.id==current_user.id).update({'email': new_email})
+        db.session.commit() # commit any changes
+
+    new_city_name =request.form.get('new_city_name')
+    if new_city_name:
+            # retrieve city_id
+        results = db.session.query(City.id).filter(City.city_name==new_city_name)
+        for r in results:
+            new_city_id = r[0]
+        db.session.query(User).filter(User.id==current_user.id).update({'city_id': new_city_id})
+        db.session.commit() # commit any changes
+        
+    new_contact_method =request.form.get('new_contact_method')
+    if new_contact_method:
+        db.session.query(User).filter(User.id==current_user.id).update({'contact_method': new_contact_method})
+        db.session.commit() # commit any changes
+
+    new_contact_info=request.form.get('new_contact_info')
+    if new_contact_info:
+        db.session.query(User).filter(User.id==current_user.id).update({'contact_info': new_contact_info})
+        db.session.commit() # commit any changes
+        
     return redirect(url_for('main_route.preference'))
-# update value for the first name: first_name
-# update value for the last name: last_name
-# update value for the email: email
-# update value for the location: city_id
+
--- web/models.py
+++ web/models.py
@@ -24,6 +24,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(200))
     contact_method = db.Column(db.String(100))
     contact_info = db.Column(db.String(300))
+    profile_img_name = db.Column(db.String(100), nullable=True)
 
 
 class Cuisine(db.Model):
@@ -110,20 +111,26 @@ def insert_dummy_users(*args, **kwargs):
         
     DummyUsers = [
             [2,'Jingren', 'Wang', 'jingren.wang@minerva.kgi.edu',
-        hashedPW[88888888], 'Phone', '+82 105-557-7494'], # Seoul
+        hashedPW[88888888], 'Phone', '+82 105-557-7494', 'user_1.jpeg'], # Seoul
             [5,'Mary', 'Burns', 'mary.burns@minerva.kgi.edu',
-        hashedPW[77777777], 'Phone', '+54 155-5516-605'], # Buenos Aires
-            [1,'James', 'Gorden', 'james.gorden@minerva.kgi.edu',
-        hashedPW[66666666], 'Phone', '+1 729-591-1038'], # San Francisco
+        hashedPW[77777777], 'Phone', '+54 155-5516-605', 'user_2.jpg'], # Buenos Aires
+            [1,'James', 'Gordon', 'james.gordon@minerva.kgi.edu',
+        hashedPW[66666666], 'Phone', '+1 729-591-1038',None ], # San Francisco
             [1,'Kia', 'Louis', 'kia.louis@minerva.kgi.edu',
-        hashedPW[55555555], 'Phone', '+1 813-028-4012'] # San Francisco
+        hashedPW[55555555], 'Phone', '+1 813-028-4012', None] # San Francisco
         ]
 
     for user_info in DummyUsers:
+        if user_info[7]:
+            user_img_name = str(user_info[7])
+        else:
+            user_img_name = 'profilePicPlaceholder.png'
+            
         db.session.add(User( city_id=user_info[0],
             first_name=user_info[1], last_name=user_info[2],
                 email=user_info[3], password=user_info[4],
-                contact_method=user_info[5], contact_info=user_info[6]))
+                contact_method=user_info[5], contact_info=user_info[6],
+                profile_img_name = user_info[7]))
     db.session.commit()
         
 
@@ -134,7 +141,7 @@ def insert_dummy_preferences(*args, **kwargs):
     db.session.add(Preference(
         date_time='2020-05-01 11:05:26',
             user_id=2, city_id=5, cuisine_id=3, dinetime_id=1,
-            require_vegan=True, start_time='11:10', end_time='13:30'
+            require_vegan=True, start_time='11:10:00', end_time='13:30:00'
             ))
             # *other 'require_' attributes default to Flase (0)
     db.session.commit()
